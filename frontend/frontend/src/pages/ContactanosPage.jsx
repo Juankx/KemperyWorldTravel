d829359ed01364ad6c99f0c8bb5f71a9a0e2f3dd
Kempery Dev <dev@kempery.com>
2026-01-21 19:12:00 -0500

    🔨 Remover Navbar/Footer duplicados de todas las páginas (PaquetesPage, NosotrosPage, ExperienciasPage, ContactanosPage)

--- frontend/frontend/src/pages/ContactanosPage.jsx
+++ frontend/frontend/src/pages/ContactanosPage.jsx
@@ -1,18 +1,12 @@
 import React from 'react'
-import Navbar from '../components/Navbar'
 import ContactForm from '../components/ContactForm'
-import Footer from '../components/Footer'
-import WhatsAppFloat from '../components/WhatsAppFloat'
 
 const ContactanosPage = () => {
   return (
     <div className="min-h-screen bg-gray-50">
-      <Navbar />
       <div className="pt-16">
         <ContactForm />
       </div>
-      <Footer />
-      <WhatsAppFloat />
     </div>
   )
 }
--- frontend/frontend/src/pages/ExperienciasPage.jsx
+++ frontend/frontend/src/pages/ExperienciasPage.jsx
@@ -1,18 +1,12 @@
 import React from 'react'
-import Navbar from '../components/Navbar'
 import Experiencias from '../components/Experiencias'
-import Footer from '../components/Footer'
-import WhatsAppFloat from '../components/WhatsAppFloat'
 
 const ExperienciasPage = () => {
   return (
     <div className="min-h-screen bg-gray-50">
-      <Navbar />
       <div className="pt-16">
         <Experiencias />
       </div>
-      <Footer />
-      <WhatsAppFloat />
     </div>
   )
 }
